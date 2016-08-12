4e723e7dcfd0020d21e6b465549967444e6c3873
dil pila <arfren.2013@gmail.com>
2016-08-12 17:40:51 +0545

    added column in post table

---
+++ database/migrations/2016_08_12_115156_add_uploadcolumn_to_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUploadcolumnToTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('file')->after('body');
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+            $table->dropColumn('file');
+        });
+    }
+}
